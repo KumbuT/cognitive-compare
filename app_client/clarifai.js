1dcc08e11d105b681811dffeaac197ab2a59dbe0
Apoorva Teja <apoorva.teja@gmail.com>
2020-03-31 23:51:49 -0700

    Added more models to clarafai

--- app_client/clarifai.js
+++ app_client/clarifai.js
@@ -20,14 +20,21 @@ var clarifaiVisionAi = {
         switch(model){
             case "Apparel": selectedModel = Clarifai.GENERAL_MODELl; break;
             case "Color": selectedModel = Clarifai.COLOR_MODEL; break;
-            case "Celebrity": selectedModel = Clarifai.FACE_DETECT_MODEL; break;
             case "FaceDetection": selectedModel = Clarifai.FACE_DETECT_MODEL; break;
             case "Food": selectedModel = Clarifai.FOOD_MODEL; break;
             case "General": selectedModel = Clarifai.GENERAL_MODELl; break;
             case "NSFW": selectedModel = Clarifai.NSFW_MODEL; break;
             case "Travel": selectedModel = Clarifai.TRAVEL_MODEL; break;
             case "Wedding": selectedModel = Clarifai.WEDDINGS_MODEL; break;
-
+            case "Apparel": selectedModel = Clarifai.APPAREL_MODEL; break;
+            case "Celebrity": selectedModel = Clarifai.CELEBRITY_MODEL; break;
+            case "Demographics": selectedModel = Clarifai.DEMOGRAPHICS_MODEL; break;
+            case "Moderation": selectedModel = Clarifai.MODERATION_MODEL; break;
+            case "Face Embed": selectedModel = Clarifai.FACE_EMBED_MODEL;break;
+            case "Landscape Quality": selectedModel = Clarifai.LANDSCAPE_QUALITY; break;
+            case "Potrait Quality": selectedModel = Clarifai.PORTRAIT_QUALITY; break;
+            case "Texture and Patterns": selectedModel = Clarifai.TEXTURES_AND_PATTERNS; break;
+            case "Logo": selectedModel = Clarifai.LOGO_MODEL; break;
         }
         app.models.predict(selectedModel, iUrl).then(
             function (response) {
